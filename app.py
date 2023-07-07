383a549daa4abac2315c270fd9e4161244996b8f
dhruv-bit01 <138872133+dhruv-bit01@users.noreply.github.com>
2023-07-07 20:35:06 +0530

    Nav bar fix and Scroll down menue
    by Dhruv and Shalya

--- app.py
+++ app.py
@@ -2,13 +2,24 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+
+DEPARTMENTS = [
+    "Techincals",
+    "Creavtives",
+    "Marketing",
+    "Logistics",
+    "RnD",
+    "SME",
+    "Photography",
+    "PR",
+]
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
 @app.route("/recruitment", methods=["GET", "POST"])
 def recruitment():
-    return render_template("recruitment.html")
+    return render_template("recruitment.html", departments = DEPARTMENTS)
 
 @app.route('/submit',methods=['POST'])
 def submit():
@@ -17,7 +28,10 @@ def submit():
     number = request.form['number']
     email = request.form['email']
     link = request.form['link']
-    print(name,course,number,email,link)
+    department1 = request.form.get('Department1')
+    department2 = request.form.get('Department2')
+    department3 = request.form.get('Department3')
+    print(name,course,number,email,link, department1, department2, department3)
     return redirect('/')
 
 if __name__ == "__main__":

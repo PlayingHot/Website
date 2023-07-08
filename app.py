434f16a297a66f9c46e7a81682e41c9bc52eb6a4
Jeshan04 <jeshxn04@gmail.com>
2023-07-08 10:43:48 +0530

    Google Sheets Works!
    Eshan and Dhruv

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, request, redirect
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pprint
 
 app = Flask(__name__)
 
+scope = [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file'
+    ]
+file_name = 'client_key.json'
+creds = ServiceAccountCredentials.from_json_keyfile_name(file_name,scope)
+client = gspread.authorize(creds)
+
+sheet = client.open('ACM_EXECS').sheet1
 
 DEPARTMENTS = [
     "Techincals",
@@ -32,6 +44,9 @@ def submit():
     department2 = request.form.get('Department2')
     department3 = request.form.get('Department3')
     print(name,course,number,email,link, department1, department2, department3)
+    row = [name, course, number, email, link, department1, department2, department3]
+    sheet.append_row(row)
+
     return redirect('/')
 
 if __name__ == "__main__":

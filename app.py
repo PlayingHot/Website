d374f6e7f80b6367c2d7711a4357022aecb4fd6e
Jeshan04 <jeshxn04@gmail.com>
2023-07-13 10:07:23 +0530

    success page added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect
+import time
+from flask import Flask, render_template, request, redirect, url_for
 import smtplib
 import ssl
 from email.message import EmailMessage
@@ -13,7 +14,7 @@ scope = [
     'https://www.googleapis.com/auth/drive.file'
     ]
 file_name = 'client_key.json'
-creds = ServiceAccountCredentials.from_json_keyfile_name(file_name,scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(file_name, scope)
 client = gspread.authorize(creds)
 
 sheet = client.open('ACM_EXECS').sheet1
@@ -28,15 +29,16 @@ DEPARTMENTS = [
     "Photography",
     "PR",
 ]
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
 @app.route("/recruitment", methods=["GET", "POST"])
 def recruitment():
-    return render_template("recruitment.html", departments = DEPARTMENTS)
+    return render_template("recruitment.html", departments=DEPARTMENTS)
 
-@app.route('/submit',methods=['POST'])
+@app.route('/submit', methods=['POST'])
 def submit():
     name = request.form['full-name']
     course = request.form['course']
@@ -46,13 +48,17 @@ def submit():
     department1 = request.form.get('Department1')
     department2 = request.form.get('Department2')
     department3 = request.form.get('Department3')
-    print(name,course,number,email,link, department1, department2, department3)
+    print(name, course, number, email, link, department1, department2, department3)
     row = [name, course, number, email, link, department1, department2, department3]
     sheet.append_row(row)
-    lis=[email]
-    cont='ACM Test2'
-    gmail(lis,cont)
-    return redirect('/')
+    lis = [email]
+    cont = 'ACM Test2'
+    gmail(lis, cont)
+    return redirect(url_for('success'))
+
+@app.route('/success', methods=['GET'])
+def success():
+    return render_template('success.html')
 
 def gmail(email_list, content):
     email_sender = 'dhruvpython10@gmail.com'
@@ -68,12 +74,11 @@ def gmail(email_list, content):
     em.set_content(body)
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
         smtp.login(email_sender, email_password)
-        for email in  email_list:
+        for email in email_list:
             em['To'] = email
             smtp.sendmail(email_sender, email, em.as_string())
             del em['To']
     return None
-
 
 if __name__ == "__main__":
     app.run(debug=True)
